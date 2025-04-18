00cbf494b37b4976967ad8a1ae3a3dd512d13fb6
Sanjay Beniwal <88551065+Sanjaybeniwal@users.noreply.github.com>
2025-04-18 21:22:57 +0530

    html work

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!")
+const showLogo = document.getElementById("logoshow");
+const logoTitle = "sanjay";
+
+let newLogo = '';
+
+for (let i = 0; i < logoTitle.length; i++) {
+    const letter = logoTitle[i];
+    
+    if (letter === "s" || letter === "y") {
+        // console.log(letter);
+        newLogo += `<span style="font-size:24px;">${letter}</span>`;
+    } else {
+        // console.log('not s or y', letter);
+        
+        newLogo += letter;
+    }
+}
+
+showLogo.innerHTML = newLogo;
